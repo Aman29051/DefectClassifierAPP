567d75b730d731c1850347e727ab7e4f6e0692cc
Aman yadav <61088050+Aman29051@users.noreply.github.com>
2023-05-07 18:01:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pickle
 import io
 import numpy as np
 from keras.models import load_model
-import streamlit_webrtc as webrtc
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 # Set page title, icon and layout
 st.set_page_config(page_title="Defect Classifier",page_icon="memo",layout="wide")
@@ -18,36 +18,29 @@ st.header("Defect Classifier Model")
 st.sidebar.image(img1)
 st.sidebar.header("Predict Defect")
 
+# Define a VideoTransformer class to capture frames from the camera
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        # Convert the frame to RGB color space
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        return frame
 
 # Function to capture image from camera
 def get_live_image():
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    if ret:
-        cap.release()
+    # Define the WebRTC video streamer
+    webrtc_ctx = webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+    # Check if the video streamer has started
+    if not webrtc_ctx.video_transformer:
+        st.warning("Error: Could not start video stream.")
+        return None
+
+    # Wait for the user to capture an image
+    st.warning("Click the 'Capture' button to take a picture.")
+    if st.button("Capture"):
+        # Capture the current frame from the video streamer
+        frame = webrtc_ctx.video_transformer.last_frame
         return frame
-    
-# Function to transform video frame to RGB format
-def transform(frame):
-    return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-# Function to predict image class
-def predict(live_image):
-    # Load model and label binarizer
-    lb = pickle.load(open("lb.pkl","rb"))
-    model = load_model("model.h5")
-
-    # Resize image and convert to numpy array
-    image = cv2.resize(live_image, (64, 64))
-    image = np.array(image)
-    image = np.reshape(image, (1, 64, 64, 3))
-
-    # Make predictions and return result
-    y_pred = model.predict(image)
-    y_predicted_labels = [np.argmax(i) for i in y_pred]
-    labels = lb.classes_
-    label = labels[y_predicted_labels[0]]
-    return label
 
 # Prompt user to select an option
 option = st.sidebar.radio("Select an option", ("Upload Image", "Take Live Image"))
@@ -86,30 +79,27 @@ if option == "Upload Image":
 
 # If "Take Live Image" is selected
 elif option == "Take Live Image":
-    # Create video transformer to capture live video from camera
-    webrtc_ctx = webrtc.StreamlitWebRTC(
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-        media_stream_constraints={"video": True, "audio": False},
-        key="camera",
-    )
-    webrtc_transformer = webrtc.VideoTransformer(
-        source_video_transformer=None,
-        transformer_factory=lambda: webrtc_opencv.VideoCaptureTransformer(transform=transform),
-        source_transformer_factory=lambda: webrtc_ctx,
-        fps=30,
-        frame_size=(640, 480),
-        allow_stream_capture=False,
-    )
-
     # Prompt user to capture image
     if st.sidebar.button("Capture and Predict"):
         # Capture image from camera
-        live_image = webrtc_transformer.last_frame
+        live_image = get_live_image()
 
         # Check if image is captured
         if live_image is not None:
             st.image(live_image, caption="Live Image", use_column_width=True)
+    
+            # Load model and label binarizer
+            lb = pickle.load(open("lb.pkl","rb"))
+            model = load_model("model.h5")
+    
+            # Resize image and convert to numpy array
+            image = cv2.resize(live_image, (64, 64))
+            image = np.array(image)
+            image = np.reshape(image, (1, 64, 64, 3))
 
             # Make predictions and display result
-            label = predict(live_image)
+            y_pred = model.predict(image)
+            y_predicted_labels = [np.argmax(i) for i in y_pred]
+            labels = lb.classes_
+            label = labels[y_predicted_labels[0]]
             st.subheader("Predicted Class : {}".format(label))
